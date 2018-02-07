fd98a2b1e31965f47834ea343a39f9c868adbfe3
Brandon York <30988963+byork2005@users.noreply.github.com>
2018-02-06 22:40:21 -0600

    working on db

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
-// var db = require("./models");
+var db = require("./models");
 
 var PORT = 3000;
 
@@ -21,46 +21,50 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
-/*
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 //  ***Need to check where mongo needs to connect***
-mongoose.connect("mongodb://localhost/week18Populater", {
+mongoose.connect("mongodb://localhost/articles", {
   useMongoClient: true
 });
-*/
 
-var data = [{title: 'Test', link: 'www.google.com'}];
+//// Routes
 
+// Home Page, will check if DB has any saved articles.
+app.get('/', function(req, res){
+    db.article.find({}).then(function(results) {
+        res.render("index", {Data: results})
+    })
+    // var articles = [];
+    // res.render("index", {Data: articles})
+})
+
+// Scrape KC Star for new articles
 app.get("/scrape", function(req, res) {
-    // axios.get("https://www.kansascity.com").then(function(response) {
-    //     var $ = cheerio.load(response.data);
-    //     var results = [{title: 'Test', link: 'www.google.com'}];
-    //     resultsTest = [{title: 'Test', link: 'www.google.com'}];
+    let results = [];
+    axios.get("https://www.kansascity.com").then(function(response) {
+        var $ = cheerio.load(response.data);
+        results = [{title: 'Results', link: 'www.google.com'}];
 
-    //     // (i: iterator. element: the current element)
-    //     $("h4.title").each(function(i, element) {
-    //         var title = $(element).text();
-    //         var link = $(element).children().attr("href");
-    //         results.push({
-    //             title: title,
-    //             link: link
-    //         });
-    //     });
+        // (i: iterator. element: the current element)
+        $("h4.title").each(function(i, element) {
+            var title = $(element).text();
+            var link = $(element).children().attr("href");
+            results.push({
+                title: title,
+                link: link
+            });
+        });
 
-    var dataObj = {
-        resultsTest: data
-    }
-    // console.log(resultsTest);
-    // console.log(results)
-    res.render("index", dataObj)
-    // });
-    // res.render('index', results)
+    console.log('Results: ',results)
+    res.render("scrape", {Data: results})
+    });
 });
 
 app.get('/', function(req, res){
-    res.render("index", { test: resultsTest})
+    var articles = [];
+    res.render("index", {Data: articles})
 })
 
 app.listen(PORT, function() {
