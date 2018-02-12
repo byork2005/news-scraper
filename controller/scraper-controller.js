023b533f1e59d08599a4d7f6d1afb0bf04f61f64
Brandon York <30988963+byork2005@users.noreply.github.com>
2018-02-12 16:43:10 -0600

    added note save and delete
    still need to test in db env.

--- controller/scraper-controller.js
+++ controller/scraper-controller.js
@@ -13,92 +13,95 @@ mongoose.Promise = Promise;
 //// Routes
 // Home Page
 router.get('/', function(req, res) {
-    db.Article.find({}).then(function(results) {
-      console.log('results: ', results);
+  db.Article.find({}).then(function(results) {
+    console.log('results: ', results);
     res.render('index', { Data: results });
-    });
   });
-  
-  // Scrape KC Star for new articles
-  router.get('/scrape', function(req, res) {
-    let results = [];
-    axios.get('https://www.kansascity.com').then(function(response) {
-      var $ = cheerio.load(response.data);
-  
-      // (i: iterator. element: the current element)
-      $('h4.title').each(function(i, element) {
-        var title = $(element).text().trim();
-        var link = $(element)
-          .children()
-          .attr('href')
-        results.push({
-          title: title,
-          link: link
-        });
-      });
-      res.render('scrape', { Data: results });
-    });
-  });
-  
-  // Save a scraped article
-  // router.post('/save', function(req, res) {
-  //   const articleObj = {};
-  //   articleObj.title = req.body.title;
-  //   articleObj.link = req.body.link;
-  //   db.Article.create(articleObj).then(function(dbArticle) {
-  //     console.log('saved: ', dbArticle);
-  //   });
-  //   res.redirect('/scrape');
-  // });
+});
 
-  router.post('/save', function(req, res) {
-    const articleObj = {};
-    articleObj.title = req.body.title;
-    articleObj.link = req.body.link;
-    db.Article.create(articleObj).then(function(dbArticle) {
-      console.log('saved: ', dbArticle);
-    });
-    res.redirect('/scrape');
-  });
-  
-  // Get Saved articles
-  router.get('/saved', function(req, res) {
-      db.Article.find({}).then(function(results) {
-        res.render('saved', { Data: results });
+// Scrape KC Star for new articles
+router.get('/scrape', function(req, res) {
+  let results = [];
+  axios.get('https://www.kansascity.com').then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    // (i: iterator. element: the current element)
+    $('h4.title').each(function(i, element) {
+      var title = $(element)
+        .text()
+        .trim();
+      var link = $(element)
+        .children()
+        .attr('href');
+      results.push({
+        title: title,
+        link: link
       });
     });
-    
-    // Get Saved article notes by ID
-    router.get('/saved/:id', function(req, res) {
-      db.Article
-        .findOne({ _id: req.params.id })
-        .populate('note')
-        .then(function(dbArticle) {
-          res.json(dbArticle);
-        })
-        .catch(function(err) {
-          res.json(err);
-        })
-    });
-  
-  // Delete a saved article
-  router.delete('/delete', function(req, res) {
-    db.Article.findByIdAndRemove(req.body.id).then(function(dbArticle) {
-      console.log('deleted: ', dbArticle);
-    });
-    res.end();
+    res.render('scrape', { Data: results });
   });
+});
 
-  router.post('/note/:id', function(req, res) {
-    db.Note.create(req.body).then(function(dbNote) {
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true })
-    })
+// Save a scraped article
+// router.post('/save', function(req, res) {
+//   const articleObj = {};
+//   articleObj.title = req.body.title;
+//   articleObj.link = req.body.link;
+//   db.Article.create(articleObj).then(function(dbArticle) {
+//     console.log('saved: ', dbArticle);
+//   });
+//   res.redirect('/scrape');
+// });
+
+router.post('/save', function(req, res) {
+  const articleObj = {};
+  articleObj.title = req.body.title;
+  articleObj.link = req.body.link;
+  db.Article.create(articleObj).then(function(dbArticle) {
+    console.log('saved: ', dbArticle);
+  });
+  res.redirect('/scrape');
+});
+
+// Get Saved articles
+router.get('/saved', function(req, res) {
+  db.Article.find({}).then(function(results) {
+    res.render('saved', { Data: results });
+  });
+});
+
+// Get Saved article notes by ID
+router.get('/saved/:id', function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate('note')
     .then(function(dbArticle) {
       res.json(dbArticle);
     })
     .catch(function(err) {
-      res.json(err)
-    })
-  })
+      res.json(err);
+    });
+});
 
-  module.exports = router
+// Delete a saved article
+router.delete('/delete', function(req, res) {
+  db.Article.findByIdAndRemove(req.body.id).then(function(dbArticle) {
+    console.log('deleted: ', dbArticle);
+  });
+  res.end();
+});
+
+// Save a note
+router.post('/note/:id', function(req, res) {
+  console.log('save body: ', req.body);
+  // db.Note.create(req.body).then(function(dbNote) {
+  //   return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true })
+  // })
+  // .then(function(dbArticle) {
+  //   res.json(dbArticle);
+  // })
+  // .catch(function(err) {
+  //   res.json(err)
+  // })
+});
+
+module.exports = router;
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,3 @@
-
 // Delete saved article after confirm message.
 
 $(document).on('click', '.delete-btn', function() {
@@ -11,13 +10,13 @@ $(document).on('click', '.delete-btn', function() {
       data: {
         id: thisArticleId
       }
-    }).then(location.reload())
+    }).then(location.reload());
   }
-})
+});
 
 // Save button. Checks if article has been saved once on this scrape.
 $(document).on('click', '.save-btn', function() {
-  if($(this).text() === 'Save') {
+  if ($(this).text() === 'Save') {
     $.ajax({
       method: 'POST',
       url: '/save',
@@ -25,76 +24,80 @@ $(document).on('click', '.save-btn', function() {
         title: $(this).attr('data-title'),
         link: $(this).attr('data-link')
       }
-    })
+    });
   }
   $(this).html('<i class="fas fa-star"></i>');
-})
+});
 
 // Opens article Notes Modal and checks for saved notes
 $(document).on('click', '.notes-btn', function() {
   const id = $(this).attr('data-id');
+  const notePanel = $(`<div class="panel panel-default">`);
+  const notePanelBody = $(`<div class="panel-body">`);
+  const deleteBtn = $(
+    `<button type="submit" class="btn btn-danger note-delete" data-id="${id}">Delete</button>`
+  );
+  const saveBtn = $(
+    `<button type="submit" class="btn btn-success note-save" data-id="${id}">Save Note</button>`
+  );
+  $('#note-btn-div').empty();
+  $('#note-body-div').empty();
+  $('#note-btn-div').append(saveBtn);
   $.ajax({
     method: 'GET',
     url: '/saved/' + id
   }).done(function(data) {
-    // const panelDiv = $('<div class="panel panel-default">')
-    // const panelBodyDiv = $('<div class="panel-body">')
-    const formButton = $(`<button type="submit" class="btn btn-success note-save" data-id="${id}">`)
-    formButton.text("Save Note")
-    $('#note-form').append(formButton);
-  })
+    console.log('data from saved notes: ', data);
+  });
   $('#myModal').modal();
-})
-
-// Saves Notes
-// $(document).on('click', '.note-save', function() {
-
-//   const id = $(this).attr('data-id');
-//   $.ajax({
-//     method: 'POST',
-//     url: '/note/' + id
-//   })
-// })
-
-$(document).on('click', '.note-save', function() {
-  // Empty the notes from the note section
-  // $("#note-body").empty();
-  // Save the id from the p tag
-  const id = $(this).attr('data-id');
-
-  // Now make an ajax call for the Article
-  $.ajax({
-    method: "GET",
-    url: "/note/" + id
-  })
-    // With that done, add the note information to the page
-    .done(function(data) {
-      console.log(data);
-      // The title of the article
-      $("#note-body").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#note-body").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
-      $("#note-body").append("<textarea id='bodyinput' name='body'></textarea>");
-
-      $('#note-body').r
-      // A button to submit a new note, with the id of the article saved to it
-      $("#note-body").append("<button data-id='" + data._id + "' class='savenote'>Save Note</button>");
-
-      // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-      }
-    });
 });
 
+// Saves Notes
+$(document).on('click', '.note-save', function() {
+  const id = $(this).attr('data-id');
 
+  $.ajax({
+    method: 'POST',
+    url: '/note/' + id
+  }).done(function(data) {
+    console.log('note save data: ', data);
+  });
+});
 
+// $(document).on('click', '.note-save', function() {
+//   // Empty the notes from the note section
+//   // $("#note-body").empty();
+//   // Save the id from the p tag
+//   const id = $(this).attr('data-id');
 
+//   // Now make an ajax call for the Article
+//   $.ajax({
+//     method: "GET",
+//     url: "/note/" + id
+//   })
+//     // With that done, add the note information to the page
+//     .done(function(data) {
+//       console.log(data);
+//       // The title of the article
+//       $("#note-body").append("<h2>" + data.title + "</h2>");
+//       // An input to enter a new title
+//       $("#note-body").append("<input id='titleinput' name='title' >");
+//       // A textarea to add a new note body
+//       $("#note-body").append("<textarea id='bodyinput' name='body'></textarea>");
 
+//       $('#note-body').r
+//       // A button to submit a new note, with the id of the article saved to it
+//       $("#note-body").append("<button data-id='" + data._id + "' class='savenote'>Save Note</button>");
+
+//       // If there's a note in the article
+//       if (data.note) {
+//         // Place the title of the note in the title input
+//         $("#titleinput").val(data.note.title);
+//         // Place the body of the note in the body textarea
+//         $("#bodyinput").val(data.note.body);
+//       }
+//     });
+// });
 
 // $(document).ready(function() {
 //   $('#myBtn').click(function() {
